0142b2afae2b0af329d7b3be701e4d895377a880
loyte-markus <32316026+loyte-markus@users.noreply.github.com>
2018-08-10 04:47:14 +0200

    Basics of bot are being made
    json file, utilities class

---
+++ ExhaustiBot/ExhaustiBot/Utilities.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace ExhaustiBot
+{
+	class Utilities
+	{
+		private static Dictionary<string, string> alerts;
+		static Utilities()
+		{
+			string json = File.ReadAllText("system/alert.json");
+			var data = JsonConvert.DeserializeObject<dynamic>(json);
+			alerts = data.ToObject<Dictionary<string, string>>();
+		}
+
+		public static string getAlert(string key)
+		{
+			if (alerts.ContainsKey(key)) return alerts[key];
+			return "";
+		}
+	}
+}
