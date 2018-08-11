ec5912402efb3b1eeb9bc56a2257eb470028cf2e
loyte-markus <32316026+loyte-markus@users.noreply.github.com>
2018-08-11 23:05:49 +0200

    3 commands added
    !hjelp: A help command showing all avaiable commands.
     - Shows all commands currently added. The list is hardcorded for now.
    
    !repeter: A command that will echo anything you say.
     - Read the message behind "!repeter" into a string and send it back.
    
    !velg: A choice helper command. !velg option1|option2|option3
      - By using Random we pick one X options and tell the user our pick.

---
+++ ExhaustiBot/ExhaustiBot/modules/Misc.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace ExhaustiBot.modules
+{
+	public class Misc : ModuleBase<SocketCommandContext>
+	{
+		[Command("hjelp")]
+		public async Task Help()
+		{
+			var embed = new EmbedBuilder();
+			embed.WithTitle("Kommandoer:");
+			embed.WithDescription("!repeter: !repeter din melding\n" +
+				"!velg: !velg utfall1|utfall2|utfall3 osv...");
+			embed.WithColor(new Color(0, 255, 0));
+			await Context.Channel.SendMessageAsync("", false, embed);
+		}
+
+		[Command("repeter")]
+		public async Task Echo([Remainder] string message)
+		{
+			var embed = new EmbedBuilder();
+			embed.WithTitle("Repetert melding:");
+			embed.WithDescription(message);
+			embed.WithColor(new Color(0, 255, 0));
+			await Context.Channel.SendMessageAsync("", false, embed);
+		}
+
+		[Command("velg")]
+		public async Task Pick([Remainder] string message)
+		{
+			string[] options = message.Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
+
+			Random r = new Random();
+			string picked = options[r.Next(0, options.Length)];
+
+			var embed = new EmbedBuilder();
+			embed.WithTitle(Context.User.Username + ", jeg velger: ");
+			embed.WithDescription(picked);
+			embed.WithColor(new Color(255, 255, 0));
+			embed.WithThumbnailUrl("https://pbs.twimg.com/media/DAAB6X4UQAEAbqM.png");
+
+			var embError = new EmbedBuilder();
+			embError.WithTitle("Jeg beklager, " + Context.User.Username);
+			embError.WithDescription("!velg krever minst to forskjellige utfall.\n" + "!velg utfall1|utfall2");
+			embError.WithColor(new Color(255, 0, 0));
+			embError.WithThumbnailUrl("https://cdn.frankerfacez.com/emoticon/119072/4");
+
+			if (options.Length > 1)
+			{
+				await Context.Channel.SendMessageAsync("", false, embed);
+			}
+			else
+			{
+				await Context.Channel.SendMessageAsync("", false, embError);
+			}
+
+		}
+	}
+}
